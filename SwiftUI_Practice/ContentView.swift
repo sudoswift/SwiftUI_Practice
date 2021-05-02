e64ca44f9b588a177881c33c23741df365bb1fd9
sudoswift <sudoswift@gmail.com>
2021-05-02 23:55:20 +0900

    Initial Commit

---
+++ SwiftUI_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Practice
+//
+//  Created by izvern on 2021/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI_Practice
+//
+//  Created by izvern on 2021/05/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
